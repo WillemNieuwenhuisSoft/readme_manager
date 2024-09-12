2a96d12fc9a7a3919a5df6c4dd9f63604bfd8e02
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-09-12 15:07:14 +0200

    Check filedate of current file, not of first backup Added logging.

--- src/bioview/save_readme_changes.py
+++ src/bioview/save_readme_changes.py
@@ -1,17 +1,20 @@
+import logging
 import os
 import datetime
+
+log = logging.getLogger(__name__)
 
 
 def save_readme_changes(filename: str, text: str) -> None:
     '''Save the changes to the readme file and create a backup
       of the previous version.'''
     today = datetime.date.today()
-    most_recent_backup = f"{filename}.{1}"
-    backup_date = os.path.getmtime(most_recent_backup)
+    backup_date = os.path.getmtime(filename)
     backup_date = datetime.date.fromtimestamp(backup_date)
     if backup_date != today:
         # first change today: create a new backup
         # Rename existing backup files by incrementing their version number
+        log.info(f"Issued backup rotation for {filename}")
         for i in range(4, 0, -1):
             old_backup = f"{filename}.{i}"
             new_backup = f"{filename}.{i+1}"
