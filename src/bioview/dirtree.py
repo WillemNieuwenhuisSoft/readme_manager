e833f39cfbaf263c51438aa5ad536cc443fcf892
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-10-22 11:11:34 +0200

    Added creation of readme or template readme files on selected folder

--- src/bioview/dirtree.py
+++ src/bioview/dirtree.py
@@ -1,9 +1,14 @@
+from datetime import datetime, date
 from importlib.resources import files
+import logging
+import shutil
 import sys
 import tkinter as tk
 from pathlib import Path
-from tkinter import ttk
+from tkinter import ttk, Menu
 from PIL import ImageTk
+
+logger = logging.getLogger(__name__)
 
 
 class DirTree(ttk.Frame):
@@ -14,15 +19,24 @@ class DirTree(ttk.Frame):
         # are not using the table feature.
         self.treeview = ttk.Treeview(self, show="tree")
         self.treeview.grid(row=0, column=0, sticky="nsew")
-        # Call the item_opened() method each item an item
-        # is expanded.
+
+        # Create the create-readme context menu
+        self.context_menu = Menu(self.treeview, tearoff=0)
+        self.context_menu.add_command(label="Create README", command=self.create_readme)
+        self.context_menu.add_command(
+            label="Create README from template", command=self.create_readme_template)
+
+        # Bind right-click to show context menu
+        self.treeview.bind("<Button-3>", self.show_context_menu)
+        # Call the item_opened() method each item an item is expanded.
         self.treeview.tag_bind(
             "fstag", "<<TreeviewOpen>>", self.item_opened)
-        # Make sure the treeview widget follows the window
-        # when resizing.
+
+        # Make sure the treeview widget follows the window when resizing.
         for w in (self, window):
             w.rowconfigure(0, weight=1)
             w.columnconfigure(0, weight=1)
+
         self.grid(row=0, column=0, sticky="nsew")
         # This dictionary maps the treeview items IDs with the
         # path of the file or folder.
@@ -36,6 +50,43 @@ class DirTree(ttk.Frame):
             root_path = Path(Path(sys.executable).anchor)
         self.load_tree(root_path)
 
+    def show_context_menu(self, event):
+        # Select the item under the cursor
+        iid = self.treeview.identify_row(event.y)
+        if iid:
+            self.treeview.selection_set(iid)
+            selected_path = self.fsobjects[iid]
+            if selected_path.is_dir():
+                self.context_menu.post(event.x_root, event.y_root)
+
+    def create_readme(self):
+        selected_item = self.treeview.selection()[0]
+        selected_path = self.fsobjects[selected_item]
+        readme_path = selected_path / "readme.txt"
+
+        if not readme_path.exists():
+            with open(readme_path, 'w') as f:
+                f.write(
+                    f"This <DATASETNAME>_readme.txt file was generated on {date.today().strftime('%Y-%m-%d')} by <NAME>\n\n")
+            self.insert_item("readme.txt", readme_path, selected_item, position=0)
+            logger.info(f"readme.txt created at {readme_path}")
+        else:
+            logger.warning(f"readme.txt already exists at {readme_path}")
+
+    def create_readme_template(self):
+        selected_item = self.treeview.selection()[0]
+        selected_path = self.fsobjects[selected_item]
+        readme_path = selected_path / "readme.txt"
+
+        template = files('animations').joinpath('readme_template.txt')
+
+        if not readme_path.exists():
+            shutil.copyfile(template, readme_path)
+            logger.info(f"Readme template copied to {readme_path}")
+            self.insert_item("readme.txt", readme_path, selected_item, position=0)
+        else:
+            logger.warning(f"readme.txt already exists at {readme_path}")
+
     def safe_iterdir(self, path: Path) -> tuple[Path, ...] | tuple[()]:
         """
         Like `Path.iterdir()`, but do not stop on permission errors.
@@ -43,7 +94,7 @@ class DirTree(ttk.Frame):
         try:
             return tuple(path.iterdir())
         except PermissionError:
-            print("You don't have permission to read", path)
+            logger.error("You don't have permission to read", path)
             return ()
 
     def get_icon(self, path: Path) -> tk.PhotoImage:
@@ -53,17 +104,22 @@ class DirTree(ttk.Frame):
         """
         return self.folder_image if path.is_dir() else self.file_image
 
-    def insert_item(self, name: str, path: Path, parent: str = "") -> str:
+    def insert_item(self, name: str, path: Path, parent: str = "", position=tk.END) -> str:
         """
         Insert a file or folder into the treeview and return the item ID.
         """
         iid = self.treeview.insert(
-            parent, tk.END, text=name, tags=("fstag",),
+            parent, position, text=name, tags=("fstag",),
             image=self.get_icon(path))
         self.fsobjects[iid] = path
         return iid
 
-    def load_tree(self, path: Path, parent: str = "") -> None:
+    def load_tree(self, path: Path) -> None:
+        # insert top-level item
+        iid = self.insert_item(path.name, path)
+        self.load_subtree(path, parent=iid)
+
+    def load_subtree(self, path: Path, parent: str = "") -> None:
         """
         Load the contents of `path` into the treeview.
         """
@@ -83,8 +139,8 @@ class DirTree(ttk.Frame):
         """
         for child_iid in self.treeview.get_children(iid):
             if self.fsobjects[child_iid].is_dir():
-                self.load_tree(self.fsobjects[child_iid],
-                               parent=child_iid)
+                self.load_subtree(self.fsobjects[child_iid],
+                                  parent=child_iid)
 
     def item_opened(self, _event: tk.Event) -> None:
         """
