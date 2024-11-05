89fd57456dd062b1132619ad1062248198c48194
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-11-05 15:51:31 +0100

    Refactored: separated write actions on readme file creation into individual functions

--- src/bioview/readme_creation.py
+++ src/bioview/readme_creation.py
@@ -1,14 +1,20 @@
 from datetime import date
+from enum import Enum
 from importlib.resources import files
 import logging
 import os
 from pathlib import Path
 from tkinter import Menu, filedialog
+from typing import TextIO
 from bioview.dirtree import DirTree
 
 
-INCLUDE_FILES: bool = True
-NO_FILE_LIST: bool = False
+class ReadmeContent(Enum):
+    EMPTY = 1
+    WITH_FILE_LIST = 2
+    TEMPLATE = 3
+    TEMPLATE_WITH_FILE_LIST = 4
+
 
 logger = logging.getLogger(__name__)
 
@@ -17,72 +23,77 @@ class ReadmeCreator:
     def __init__(self, directory_tree: DirTree):
         self.dir_tree = directory_tree
 
+    def create_file_list(self, folder: Path) -> list[Path]:
+        return folder.iterdir()
+
     def create_readme_context_menu(self):
         # Create the create-readme context menu
         self.context_menu = Menu(self.dir_tree.treeview, tearoff=0)
         self.context_menu.add_command(
-            label="Create empty README", command=lambda f=NO_FILE_LIST: self.create_readme(f))
+            label="Create empty README",
+            command=lambda f=ReadmeContent.EMPTY: self.create_readme(f))
         self.context_menu.add_command(
-            label="Create README with file list", command=lambda f=INCLUDE_FILES: self.create_readme(f))
+            label="Create README with file list",
+            command=lambda f=ReadmeContent.WITH_FILE_LIST: self.create_readme(f))
         self.context_menu.add_command(
-            label="Create README from template", command=lambda f=NO_FILE_LIST: self.create_readme_template(f))
+            label="Create README from template",
+            command=lambda f=ReadmeContent.TEMPLATE: self.create_readme(f))
         self.context_menu.add_command(
-            label="Create README from template/file list", command=lambda f=INCLUDE_FILES: self.create_readme_template(f))
+            label="Create README from template/file list",
+            command=lambda f=ReadmeContent.TEMPLATE_WITH_FILE_LIST: self.create_readme(f))
 
         return self.context_menu
 
-    def create_readme(self, include_file_list: bool = False) -> Path | bool:
+    def init_readme_file(self) -> Path | None:
         selected_path = self.dir_tree.get_selected_path()
         readme_path = selected_path / "readme.txt"
-
         file = filedialog.asksaveasfile(
             initialfile=readme_path,
             defaultextension=".txt",
             filetypes=[("Readme files", "*.txt"), ("All files", "*.*")]
         )
         if not file:
+            return None
+
+        file.close()
+
+        return Path(file.name)
+
+    def write_header(self, file: TextIO) -> None:
+        user = os.getlogin()
+        file.write(f"This {file.name} file was generated on {
+            date.today().strftime('%Y-%m-%d')} by {user}\n\n")
+
+    def write_file_list(self, file: TextIO, file_list: list[Path]) -> None:
+        for f in file_list:
+            file.write(f"{f.name}\n")
+        file.write("\n")
+
+    def write_template(self, file: TextIO, file_list: list[Path]):
+        template_name = files('animations').joinpath('readme_template.txt')
+        with open(template_name, 'r') as template:
+            template_content = template.readlines()
+            for line in template_content:
+                file.write(f"{line}")
+                if line.lower().startswith('file list'):
+                    self.write_file_list(file, file_list)
+
+    def create_readme(self, content: ReadmeContent) -> None:
+        file_path = self.init_readme_file()
+        if not file_path:
             return False
 
-        user = os.getlogin()
-        file_path = Path(file.name)
-        file.write(f"This {file_path.name} file was generated on {
-                   date.today().strftime('%Y-%m-%d')} by {user}\n\n")
-        if include_file_list:
-            files = file_path.parent.iterdir()
-            for f in files:
-                file.write(f"{f.name}\n")
-        file.close()
+        file_list = []
+        if content in [ReadmeContent.WITH_FILE_LIST, ReadmeContent.TEMPLATE_WITH_FILE_LIST]:
+            file_list = self.create_file_list(self.dir_tree.get_selected_path())
+
+        with open(file_path, 'w') as file:
+            self.write_header(file)
+            if content in [ReadmeContent.TEMPLATE, ReadmeContent.TEMPLATE_WITH_FILE_LIST]:
+                self.write_template(file, file_list)
+            elif content == ReadmeContent.WITH_FILE_LIST:
+                self.write_file_list(file, file_list)
 
         folder_id = self.dir_tree.get_selected_id()
         self.dir_tree.insert_item(file_path.name, file_path, folder_id, position=0)
         logger.info(f"{file_path.name} created at {file_path}")
-
-        return file_path
-
-    def create_readme_template(self, include_file_list: bool = False) -> bool:
-        # do not add file list yet, it will be added later
-        new_file = self.create_readme(include_file_list=False)
-        if not new_file:
-            return False
-
-        template = files('animations').joinpath('readme_template.txt')
-
-        files_in_folder = []
-        if include_file_list:
-            files_in_folder = new_file.parent.iterdir()
-
-        with open(template, 'r') as f:
-            template_content = f.readlines()
-            with open(new_file, 'a') as new_f:
-                for line in template_content:
-                    new_f.write(f"{line}")
-                    if line.lower().startswith('file list'):
-                        for f in files_in_folder:
-                            new_f.write(f"{f.name}\n")
-                        new_f.write("\n")
-
-            logger.info(f"Readme template copied to {new_file}")
-            folder_id = self.dir_tree.get_selected_id()
-            self.dir_tree.insert_item("readme.txt", new_file, folder_id, position=0)
-
-        return True
