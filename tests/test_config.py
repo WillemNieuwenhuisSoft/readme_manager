b701e0dd7352533bbc44a6631277f2acd2758f1e
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-11-07 15:28:30 +0100

    Added tests for Config object

---
+++ tests/test_config.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+from unittest import mock
+import pytest
+from bioview.config import Config, CONFIG_FILE
+
+
+@pytest.fixture
+def config_data():
+    return {
+        "WorkFolder": str(Path("/path/to/workfolder")),
+        "MRU": [str(Path(f"/path/to/recent{i}")) for i in range(5)],
+        "active_template": str(Path("/path/to/active_template.txt"))
+    }
+
+
+@mock.patch('builtins.open')
+@mock.patch.object(Path, 'exists', return_value=True)
+def test_load_with_existing_config(mock_exists, mock_open, config_data):
+    mock_open.return_value.__enter__.return_value.read.return_value = json.dumps(
+        config_data)
+
+    config = Config(WorkFolder=Path("/default/workfolder"))
+    config.load()
+
+    assert config.WorkFolder == Path(config_data["WorkFolder"])
+    assert config.MRU == [Path(p) for p in config_data["MRU"]]
+    assert config.active_template == Path(config_data["active_template"])
+
+
+@mock.patch('builtins.open')
+@mock.patch.object(Path, 'exists', return_value=False)
+def test_load_with_no_config(mock_exists, mock_open):
+    config = Config(WorkFolder=Path("/default/workfolder"))
+    config.load()
+
+    assert config.WorkFolder == Path("/default/workfolder")
+    assert config.MRU == [Path() for _ in range(5)]
+    assert config.active_template == config.all_templates[0]
