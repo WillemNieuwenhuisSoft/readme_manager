ec4d47bbe3d09d755dc35f59400314bda59cc14d
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-11-07 15:29:32 +0100

    Fixed test because of interface change (from str to Path)

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,23 +1,24 @@
+from pathlib import Path
 from bioview.gui import pretty_print, pretty_print_name
 from bioview.load_readme import read_file_contents
 
 
 def test_pretty_print_with_short_path():
-    path = "C:\\Users\\John\\Documents\\file.txt"   # length == 32
+    path = Path("C:\\Users\\John\\Documents\\file.txt")   # length == 32
     max_length = 30
-    expected_output = "C:\\Users\\John\\D\\file.txt"  # length == 24
+    expected_output = Path("C:\\Users\\John\\D\\file.txt")  # length == 24
     assert pretty_print(path, max_length) == expected_output
 
 
 def test_pretty_print_with_long_path():
-    path = "C:\\Users\\John\\Documents\\Folder1\\Folder2\\Folder3\\file.txt"
+    path = Path("C:\\Users\\John\\Documents\\Folder1\\Folder2\\Folder3\\file.txt")
     max_length = 30
-    expected_output = "C:\\Users\\John\\D\\F\\F\\F\\file.txt"
+    expected_output = Path("C:\\Users\\John\\D\\F\\F\\F\\file.txt")
     assert pretty_print(path, max_length) == expected_output
 
 
 def test_pretty_print_name():
-    path = "C:\\Users\\John\\Documents\\file.txt"
+    path = Path("C:\\Users\\John\\Documents\\file.txt")
     max_length = 30
-    expected_output = "file.txt"
+    expected_output = Path("file.txt")
     assert pretty_print_name(path, max_length) == expected_output
--- tests/test_load_readme.py
+++ tests/test_load_readme.py
@@ -1,35 +1,39 @@
+from pathlib import Path
 import mock
-# from bioview.load_readme import read_file_contents
 import bioview.load_readme as load_readme
 
 
 @mock.patch('builtins.open')
+@mock.patch('bioview.load_readme.Path.stat')
 @mock.patch('bioview.load_readme.CharsetDetector.get_charset')
-def test_loadReadmeFile_with_valid_encoding(mock_get_charset, mock_open):
+def test_loadReadmeFile_with_valid_encoding(mock_get_charset, mock_stat, mock_open):
     # Mock the file contents and encoding
     file_contents = "This is a test file."
     encoding = "utf-8-sig"
     mock_get_charset.return_value = encoding
+    mock_stat.return_value.st_size = 20
     mock_open.return_value.__enter__.return_value.read.return_value = file_contents
 
     # Call the method
-    actual_contents = load_readme.read_file_contents("test_file.txt")
+    actual_contents = load_readme.read_file_contents(Path("test_file.txt"))
 
     # Assert that the textfield contains the expected contents
     assert actual_contents == file_contents
 
 
 @mock.patch('builtins.open')
+@mock.patch('bioview.load_readme.Path.stat')
 @mock.patch('bioview.load_readme.CharsetDetector.get_charset')
-def test_loadReadmeFile_with_invalid_encoding(mock_get_charset, mock_open):
+def test_loadReadmeFile_with_invalid_encoding(mock_get_charset, mock_stat, mock_open):
     # Mock the file contents and encoding
     file_contents = "This is a test file."
     encoding = None
     mock_open.return_value.__enter__.return_value.read.return_value = file_contents
     mock_get_charset.return_value = encoding
+    mock_stat.return_value.st_size = 20
 
     # Call the method
-    actual_contents = load_readme.read_file_contents("test_file.txt")
+    actual_contents = load_readme.read_file_contents(Path("test_file.txt"))
 
     # Assert that the textfield contains the error message
     expected_contents = "Error: Unable to decode file."
