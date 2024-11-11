7c5af095a58f4e0ac8603d7128167f876d0bce0e
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-11-11 15:12:26 +0100

    Remove unused function

--- src/bioview/load_readme_list.py
+++ src/bioview/load_readme_list.py
@@ -2,21 +2,6 @@ from pathlib import Path
 import pandas as pd
 
 
-def load_list_from_excel(file_path: Path) -> pd.Series:
-    """
-    Load a list readme file locations from an Excel file.
-
-    Args:
-        file_path (str): The path to the Excel file.
-
-    Returns:
-        series: a list containing the file locations of all readme files.
-    """
-    # Load the Excel file
-    data = pd.read_excel(file_path, sheet_name='All readme files')
-    return data['Path'] + '/' + data['Name']
-
-
 def load_list_from_text(file_path: Path) -> pd.Series:
     """
     Load a list readme file locations from a text file.
