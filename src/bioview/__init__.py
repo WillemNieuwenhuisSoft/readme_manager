f64faf300bd0094a8a01ef8e212fda518c188492
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2024-11-14 10:23:03 +0100

    Include date and time in command line logger

--- src/bioview/__init__.py
+++ src/bioview/__init__.py
@@ -7,6 +7,10 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 log = logging.getLogger(__name__)
+logFormatter = logging.Formatter(
+    "%(asctime)s [%(levelname)-5.5s]  %(message)s")
+logFormatter.datefmt = "%Y-%m-%d %H:%M:%S"
 ch = logging.StreamHandler()
+ch.setFormatter(logFormatter)
 ch.setLevel(logging.INFO)
 log.addHandler(ch)
